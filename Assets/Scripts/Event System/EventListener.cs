76bd722d15c1a0d28ac17b423a433fb54debb0d1
Joxev <jackqjamison@gmail.com>
2021-01-11 16:43:51 -0500

    Imported Yarn and Event System

---
+++ Assets/Scripts/Event System/EventListener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public UnityEvent Response;
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+    void OnEnable()
+    {
+        Event.Register(this);
+    }
+    void OnDisable()
+    {
+        Event.DeRegister(this);
+    }
+}
