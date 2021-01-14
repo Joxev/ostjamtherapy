bde61ac2a73eb44b140947ffafa85f69d43c9958
MaxTrollinger <70787821+MaxTrollinger@users.noreply.github.com>
2021-01-13 21:21:38 -0500

    Added More Functionality and Completed Dialogue With Emotion Values
    Emotion Values
    Animations
    Anger And Sadness Systems
    Death And Player Death
    Game Manager

---
+++ Assets/Core/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Patient> patients = new List<Patient>();
+
+    int currentPatient = 0;
+    int suicideCount = 0;
+
+    [Header("Effects")]
+    public GameObject normalPostProcessing;
+    public GameObject deathPostProcessing;
+
+    private void Start()
+    {
+        foreach(Patient p in patients)
+        {
+            if(p.gameObject.activeSelf)
+            {
+                p.gameObject.SetActive(false);
+            }
+        }
+        deathPostProcessing.SetActive(false);
+        normalPostProcessing.SetActive(true);
+
+        BubbleDialogueUI.instance.dialogueRunner.AddCommandHandler("PatientSuicide", patientSuicide);
+        BubbleDialogueUI.instance.dialogueRunner.AddCommandHandler("PlayerDeath", playerDeath);
+        BubbleDialogueUI.instance.dialogueRunner.AddCommandHandler("PatientSuccess", patientSuccess);
+
+        initalizePatient();
+    }
+    public void initalizePatient()
+    {
+        patients[currentPatient].gameObject.SetActive(true);
+        patients[currentPatient].initalizePatient();
+    }
+
+    public void nextPatient()
+    {
+        if(currentPatient >= patients.Count)
+        {
+            //Game Over
+            return;
+        }
+        patients[currentPatient].gameObject.SetActive(false);
+        currentPatient++;
+
+        initalizePatient();
+    }
+    public void patientSuccess(string[] info)
+    {
+
+    }
+    public void patientSuicide(string[] info)
+    {
+        StartCoroutine(deathWait());
+    }
+    IEnumerator deathWait()
+    {
+        deathPostProcessing.SetActive(true);
+        normalPostProcessing.SetActive(false);
+        yield return new WaitForSeconds(5f);
+
+        suicideCount++;
+        
+        if(suicideCount >= 2)
+        {
+            //Game Over
+        }
+        else
+        {
+            nextPatient();
+            deathPostProcessing.SetActive(false);
+            normalPostProcessing.SetActive(true);
+        }
+    }
+
+    public void playerDeath(string[] info)
+    {
+        deathPostProcessing.SetActive(true);
+        normalPostProcessing.SetActive(false);
+        //GameOver
+    }
+
+
+}
--- Assets/Core/Scripts/OptionButton.cs
+++ Assets/Core/Scripts/OptionButton.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class OptionButton : MonoBehaviour
+public class OptionButton : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject clickPrefab;
     public GameObject canvas;
     public TextMeshProUGUI buttonText;
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Animator>().SetBool("isHover", true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Animator>().SetBool("isHover", false);
+    }
     public void onClicked()
     {
         TextMeshProUGUI t = Instantiate(clickPrefab, transform.position, transform.rotation, canvas.transform).GetComponent<TextMeshProUGUI>();
